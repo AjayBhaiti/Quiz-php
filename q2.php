9bbcc4ef7353d12e91536783aeaee7e23c1796bc
AjayBhaiti <52485890+AjayBhaiti@users.noreply.github.com>
2020-04-11 06:45:29 +0530

    Question 2

--- q2.php
+++ q2.php
@@ -4,7 +4,7 @@
 	<title>Q2</title>
 </head>
 <body>
-	<p>What is the correct way to create a function in PHP?</p>
+	<h1 class="larger">What is the correct way to create a function in PHP?</h1>
 	<form action="connection.php" method="POST">
 		<input type="radio" name="q" value=1> function myFunction() <br>
 		<input type="radio" name="q" value=2> new_function myFunction()  <br>
